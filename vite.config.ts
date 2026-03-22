1dc965635fca8621c80207e072af6cc1fcfd4197
ximpanzemedrozo-dev <ximpanzemedrozo@gmail.com>
2026-03-22 03:00:04 -0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  // Garante que os caminhos dos arquivos gerados (JS/CSS) sejam relativos, resolvendo o erro text/html
-  base: './', 
-});
